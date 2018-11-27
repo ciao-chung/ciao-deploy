8a64fa8c4228ea0a05e2b1121fbbbddf356a360b
Ciao Chung <ciao0958@gmail.com>
2018-11-27 23:18:05 +0800

    fix: auto agree install when let's encrypt will be installed

--- src/Commands/Env/WebServer/LetsEncrypt.js
+++ src/Commands/Env/WebServer/LetsEncrypt.js
@@ -2,7 +2,7 @@ const BaseCommand = require('../../BaseCommand')
 class LetsEncrypt extends BaseCommand{
   async exec() {
     await execAsync(`sudo apt-get update`)
-    await execAsync(`sudo apt-get install python-letsencrypt-apache`)
+    await execAsync(`sudo apt-get install python-letsencrypt-apache -y`)
     await execAsync(`sudo wget https://dl.eff.org/certbot-auto`)
     await execAsync(`sudo mv certbot-auto ${certbotPath}`)
     await execAsync(`sudo chmod a+x certbot-auto`)
