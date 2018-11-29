4edcf7cf68c23d62b2cc86da997a9bd33ed3953c
Ciao Chung <ciao0958@gmail.com>
2018-11-29 23:27:15 +0800

    fix: make mysql create user command can create multiple users at one time

--- src/Commands/Env/MySqlCreateUser.js
+++ src/Commands/Env/MySqlCreateUser.js
@@ -2,12 +2,12 @@ const BaseCommand = require('../BaseCommand')
 class MySqlCreateUser extends BaseCommand{
   async exec() {
     try {
-      const username = config.mysql.username
-      const password = config.mysql.password
       const rootPassword = config.mysql.rootPassword
       await this.shelljs.env['MYSQL_PWD'] = rootPassword
-      await execAsync(`mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${username}'@'%' IDENTIFIED BY '${password}' WITH GRANT OPTION"`)
-      await execAsync(`mysql -uroot -e "FLUSH PRIVILEGES"`)
+      for(const user of config.mysql.createUsers) {
+        await execAsync(`mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${user.username}'@'%' IDENTIFIED BY '${user.password}' WITH GRANT OPTION"`)
+        await execAsync(`mysql -uroot -e "FLUSH PRIVILEGES"`)
+      }
     } catch(error) {
       log(`Create MySQL user fail: ${JSON.stringify(error)}`, 'red')
     }
