db8a1cf739da5f31d2b63cbac3694499572f766b
Ciao Chung <ciao0958@gmail.com>
2019-03-14 20:38:09 +0800

    feat: fish安裝後直接安裝oh-my-fish的git status theme

--- app/src/Env/Base.js
+++ app/src/Env/Base.js
@@ -1,6 +1,8 @@
 class Base {
   async exec() {
     await execAsync(`sudo apt-get update`)
+    await execAsync(`sudo apt-get install git -y`)
+    await execAsync(`sudo apt-get install tig -y`)
     await execAsync(`sudo apt-get install vim -y`)
     await execAsync(`sudo apt-get install curl -y`)
     await execAsync(`sudo apt-get install xclip -y`)
